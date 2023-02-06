a4c73879f4a076514db63df25e0dd3e82e9d1ddc
DEYVID LEONEL <85375493+deyvidAllan@users.noreply.github.com>
2023-02-06 15:35:56 -0300

    Create Conversor BRL|| US$.py

---
+++ Conversor BRL|| US$.py
@@ -0,0 +1,3 @@
+carteira =float(input('Quanto dinheiro vc tem em carteira? R$'))
+dollar = carteira / 5.16
+print('Com o total de R$: {} em carteira vc tem: US$ {:.2f} em dollar'.format(carteira, dollar))
